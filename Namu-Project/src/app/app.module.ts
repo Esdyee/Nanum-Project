b4589160a220ec249e9e53c3545ebf7785f2f631
cmygray <cmygray@gmail.com>
2017-11-29 14:41:11 +0900

    pull from upstream-dev

--- Namu-Project/src/app/app.module.ts
+++ Namu-Project/src/app/app.module.ts
@@ -2,16 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MaterialModule } from './module/material.module'
 
-<<<<<<< HEAD
-// material
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatCardModule, MatCheckboxModule } from '@angular/material';
-
-// frame
-=======
-//component
->>>>>>> e43564b28fbeeade386f3df7847898b81a13d019
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { TopicComponent } from './topic/topic.component';
@@ -20,7 +10,7 @@ import { ProfileComponent } from './profile/profile.component';
 import { MainFeedContentComponent } from './common/main-feed-content/main-feed-content.component';
 import { NavigatorComponent } from './common/navigator/navigator.component';
 
-//pipe
+// pipe
 import { ImagePathPipe } from './pipe/image-path.pipe';
 
 @NgModule({
@@ -35,14 +25,7 @@ import { ImagePathPipe } from './pipe/image-path.pipe';
     ImagePathPipe
   ],
   imports: [
-<<<<<<< HEAD
-    BrowserModule,
-    BrowserAnimationsModule,
-    NoopAnimationsModule,
-    MatButtonModule, MatCheckboxModule, MatCardModule
-=======
     BrowserModule, MaterialModule
->>>>>>> e43564b28fbeeade386f3df7847898b81a13d019
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Namu-Project/src/app/module/material.module.ts
+++ Namu-Project/src/app/module/material.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 
-//material
+// material
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 import { MatSliderModule } from '@angular/material/slider';
+import { MatCardModule } from '@angular/material';
 
 @NgModule({
   exports: [NoopAnimationsModule,
     MatButtonModule, MatCheckboxModule, MatSliderModule,
-    BrowserAnimationsModule],
+    BrowserAnimationsModule, MatCardModule],
   declarations: []
 })
 export class MaterialModule { }
