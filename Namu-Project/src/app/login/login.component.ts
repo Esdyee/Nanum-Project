dc7cb9964407c2c0080950ee11fdedadceb6e71b
Esdyee <joimh@naver.com>
2017-11-30 22:52:13 +0900

    layer 수정 및 app.module에 로그인 관련 컴포넌트 추가
    layer 수정 및 app.module에 로그인 관련 컴포넌트 추가

--- Namu-Project/src/app/login/login.component.ts
+++ Namu-Project/src/app/login/login.component.ts
@@ -4,18 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-login',
   //templateUrl: './login.component.html',
   template: `
-  <div class="back-image" [style.backgroundImage]="''">
-    <div class="layer">
-      <p>이제 전문적인 정보를 찾으로 헤메지 마세요.</p>
-      <p>지식 공유 네트워크, 나눔입니다.</p>
-      <button>로그인</button>
-      <button>페이스북으로 로그인</button>
-      <div class="account">
-        <button>비밀번호 찾기</button> |
-        <button>회원가입</button>
-      </div>
-    </div>
-  </div>
+  <app-main-login></app-main-login>
   `,
   styleUrls: ['./login.component.css']
 })
