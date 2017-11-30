7e493e6306a0506b5bf2a0d2350ea4e037f2cfb0
Esdyee <joimh@naver.com>
2017-11-30 22:45:50 +0900

    request Error 수정
    request Error 수정

--- Namu-Project/src/app/app.component.ts
+++ Namu-Project/src/app/app.component.ts
@@ -3,12 +3,6 @@ import { Component } from '@angular/core';
 @Component ({
   selector: 'app-root',
   templateUrl: './app.component.html',
-<<<<<<< HEAD
-  // template: `
-  //   <app-answer></app-answer>
-  // `,
-=======
->>>>>>> KimWon#10
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
