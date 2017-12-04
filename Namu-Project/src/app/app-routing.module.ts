6bcfdd350f7f54e0c0bcc59b44f739b8c34d5884
Esdyee <joimh@naver.com>
2017-12-04 14:16:02 +0900

    코멘트 추가
    코멘트 추가

--- Namu-Project/src/app/app-routing.module.ts
+++ Namu-Project/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { LoginModule } from './login/login.module';
 import { LoginComponent } from './login/login.component';
+//라우트에 해당되는 것이 없을 때 나타나는 페이지
 import { NotFoundComponent } from './common/not-found/not-found.coponent';
 
 // 라우트 구성
 const routes: Routes = [
   { path: '', redirectTo: '/login/main', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
+  //필수 추가 요망
   { path: '**', component: NotFoundComponent}
 ];
 
