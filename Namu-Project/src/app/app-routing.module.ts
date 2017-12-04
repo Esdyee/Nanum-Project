0cd0abf8e3783a6329886e98d1130626856c1288
cmygray <cmygray@gmail.com>
2017-12-04 16:29:19 +0900

    main-feed component 라우터에 추가

--- Namu-Project/src/app/app-routing.module.ts
+++ Namu-Project/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { LoginModule } from './login/login.module';
 import { LoginComponent } from './login/login.component';
+import { MainFeedComponent } from './main-feed/main-feed.component';
 //라우트에 해당되는 것이 없을 때 나타나는 페이지
 import { NotFoundComponent } from './common/not-found/not-found.coponent';
 
@@ -10,7 +11,9 @@ import { NotFoundComponent } from './common/not-found/not-found.coponent';
 const routes: Routes = [
   { path: '', redirectTo: '/login/main', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  //필수 추가 요망
+  // 필수 추가 요망
+  // 메인피드 임시 path 입니다
+  { path: 'main', component: MainFeedComponent },
   { path: '**', component: NotFoundComponent}
 ];
 
