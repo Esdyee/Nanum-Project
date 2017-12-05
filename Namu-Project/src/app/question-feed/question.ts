ea04a44c217a7220a95fd806cb277c2152b5be98
cmygray <cmygray@gmail.com>
2017-12-06 03:05:43 +0900

    dummy data 정리

--- Namu-Project/src/app/question-feed/question.ts
+++ Namu-Project/src/app/question-feed/question.ts
@@ -21,25 +21,3 @@ export const questions = [
     question: '박찬호 선수에게 싸인을 받고 싶어요'
   },
 ];
-
-/*
-export class Question {
-  [id: number]: {
-    user: User;
-    content: string;
-  }
-}
-
-export const questions = {
-  1: {
-    user: users.me,
-    qustion: 'Angular 프레임워크는 무엇인가요?'},
-  2: {
-    user: users.me,
-    qustion: 'Facebook 광고는 어떻게 시작하나요?'},
-  3: {
-    user: users.me,
-    qustion: '박찬호 선수에게 싸인을 받고 싶어요'
-  },
-};
-*/
