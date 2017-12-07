96cadf9142e65a8912a922e46a19b3634dfa5b16
Esdyee <joimh@naver.com>
2017-12-07 23:43:08 +0900

    feature 임시저장
    feature 임시저장

--- Namu-Project/src/app/login/login-connect/login-connect.component.ts
+++ Namu-Project/src/app/login/login-connect/login-connect.component.ts
@@ -65,8 +65,6 @@ export class LoginConnectComponent implements OnInit {
   }
 
   onSubmit(){
-    console.log(1);
-    console.log(this.emailFormControl.value);
     this.auth.id = this.emailFormControl.value;
     this.auth.password = this.passwordFormControl.value;
   }
