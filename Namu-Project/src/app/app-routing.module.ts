e18d420bdc628ae25030281ec5e507646de35995
cmygray <cmygray@gmail.com>
2017-12-09 04:30:36 +0900

    update

--- Namu-Project/src/app/app-routing.module.ts
+++ Namu-Project/src/app/app-routing.module.ts
@@ -13,7 +13,7 @@ import { AnswerComponent } from './answer/answer.component';
 
 // 라우트 구성
 const routes: Routes = [
-  { path: '', redirectTo: '', pathMatch: 'full' },
+  { path: '', redirectTo: 'login/main', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'main', component: MainFeedComponent, canActivate: [LoginGuard]  },
   { path: '**', component: NotFoundComponent},
