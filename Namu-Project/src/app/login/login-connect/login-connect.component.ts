38c59a93aa4119feea715ee50bab3ed16d3b8bd6
Esdyee <joimh@naver.com>
2017-12-08 21:42:18 +0900

    로그인 임시 저장
    로그인 임시 저장

--- Namu-Project/src/app/login/login-connect/login-connect.component.ts
+++ Namu-Project/src/app/login/login-connect/login-connect.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators, FormGroupDirective, NgForm } from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
-import { LoginGuard } from '../login.guard';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+import { AppService } from '../../app.service';
+import { AuthService } from '../auth.service';
+import 'rxjs/add/operator/map';
 
 /** Error when invalid control is dirty, touched, or submitted. */
 export class MyErrorStateMatcher implements ErrorStateMatcher {
@@ -35,13 +40,18 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
         <input type="password" matInput placeholder="Password" [formControl]="passwordFormControl"
                [errorStateMatcher]="matcher">
         <mat-hint>비밀번호를 입력해주세요.</mat-hint>
-        <mat-error *ngIf="passwordFormControl.hasError('required')">
+        <mat-error *ngIf="error && passwordFormControl.hasError('required')">
+          이메일 혹은 비밀번호가 일치하지 않습니다.
+        </mat-error>
+        <mat-error *ngIf="!error && passwordFormControl.hasError('required')">
           비밀번호를 <strong>입력</strong>해 주시기 바랍니다.
         </mat-error>
       </mat-form-field>
-
+      <button type="submit" class="btn-login" mat-raised-button>로그인</button>
+      {{error}}
+      {{!passwordFormControl.hasError('required')}}
     </form>
-    <button class="btn-login" mat-raised-button routerLink="../../main">로그인</button>
+    
   </section>
   `,
   styleUrls: ['./login-connect.component.css']
@@ -49,8 +59,9 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
 export class LoginConnectComponent implements OnInit {
   loginForm: FormGroup;
   matcher = new MyErrorStateMatcher();
+  error:string = '';
 
-  constructor(public auth : LoginGuard) { }
+  constructor(private http: HttpClient, private path: AppService, private auth: AuthService, private router:Router) { }
 
   ngOnInit() {
     this.loginForm = new FormGroup({
@@ -59,13 +70,30 @@ export class LoginConnectComponent implements OnInit {
         Validators.email
       ]),
       passwordFormControl: new FormControl('', [
-        Validators.required
+        Validators.required,
       ])
     });
   }
 
   onSubmit(){
-    
+    //this.loading = true;
+    this.auth.login(this.emailFormControl.value, this.passwordFormControl.value)
+      .subscribe(result => {
+        if (result === true) {
+          // login successful
+          this.router.navigate(['/main']);
+        } else {
+          // login failed
+          this.error = 'Username or password is incorrect';
+          //this.loading = false;
+        }
+      }, err => {
+        console.log(err);
+        this.error = JSON.parse(err._body).email[0];
+        //this.loginForm.reset();
+        console.log(this.loginForm);
+        this.loginForm.patchValue({ passwordFormControl:""})
+      });
   }
 
   //Form Data Return
