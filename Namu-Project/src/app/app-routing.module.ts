956f148106aeebdf0c62127dffd7e3d1aa0165e1
cmygray <cmygray@gmail.com>
2017-12-10 23:57:21 +0900

    app file 수정 1. 개발용 routing 추가 (questions, editor) 2. quill 관련 rx모듈 추가 3. app.service.ts 오타 수정

--- Namu-Project/src/app/app-routing.module.ts
+++ Namu-Project/src/app/app-routing.module.ts
@@ -10,12 +10,15 @@ import { NotFoundComponent } from './common/not-found/not-found.component';
 import { QuestionFeedComponent } from './question-feed/question-feed.component';
 import { NavigatorComponent } from './common/navigator/navigator.component';
 import { AnswerComponent } from './answer/answer.component';
+import { EditorComponent } from './common/editor/editor.component';
 
 // 라우트 구성
 const routes: Routes = [
   { path: '', redirectTo: 'login/main', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'main', component: MainFeedComponent, canActivate: [LoginGuard]  },
+  { path: 'main', component: MainFeedComponent, canActivate: [LoginGuard] },
+  { path: 'questions', component: QuestionFeedComponent },
+  { path: 'editor', component: EditorComponent },
   { path: '**', component: NotFoundComponent},
 ];
 
