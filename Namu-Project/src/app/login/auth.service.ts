43ac5413588067eaeb69c0a1418b444f8f2b9933
cmygray <cmygray@gmail.com>
2017-12-12 16:45:27 +0900

    fix typo

--- Namu-Project/src/app/login/auth.service.ts
+++ Namu-Project/src/app/login/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http, Headers, Response } from '@angular/http';
 import { Observable } from 'rxjs';
 import { AppService } from '../app.service';
-import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class AuthService {
