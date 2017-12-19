e022c777a563ed506134ec7ad4bde054c1fca189
Esdyee <joimh@naver.com>
2017-12-19 12:09:49 +0900

    facebook 로그인 일부완성
    facebook 로그인 일부완성

--- Nanum-Project/src/app/login/main-login/main-login.component.ts
+++ Nanum-Project/src/app/login/main-login/main-login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
+import { Observable } from 'rxjs';
 import { AuthService } from '../auth.service';
 import { AppService } from '../../app.service';
 
@@ -11,15 +13,24 @@ declare var FB: any;
   styleUrls: ['./main-login.component.css']
 })
 export class MainLoginComponent implements OnInit {
-  obj: { };
+  obj: {};
   test: string;
+  loginOK: Observable<object>;
 
-  constructor(private route: ActivatedRoute, private navRoute: Router, private auth: AuthService, private path: AppService) {   }
+  constructor(private route: ActivatedRoute, private navRoute: Router, private auth: AuthService, private path: AppService) {
+    // facebook login 상태
+    this.auth.login$.takeWhile(() => this.auth.sign)
+      .finally(() => this.navigateMain())
+      .subscribe( name => {
+      console.log(name);
+      this.loginOK = name;
+      this.navigateMain();
+    } );
+  }
 
   ngOnInit() {
-    console.log('[ONINIT]', this.auth);
-    // if(localStorage.getItem('currentUser')){
     //   this.navRoute.navigate(['/main']);
+    // if(localStorage.getItem('currentUser')){
     // }
   }
 
@@ -28,44 +39,11 @@ export class MainLoginComponent implements OnInit {
   }
 
   FBLogin() {
-    this.auth.facebooklogin();
-    // const funcPost = function (fbtoken, userid) {
-    //   return new Promise((resolve, reject) => {
-    //     const req = new XMLHttpRequest();
-    //     req.open('POST', 'https://siwon.me/user/facebook-login/');
-    //     req.setRequestHeader('Content-type', 'application/json');
-    //     req.send(JSON.stringify({ access_token: fbtoken, facebook_user_id: userid }));
-    //     req.onreadystatechange = function () {
-    //       if (req.readyState === XMLHttpRequest.DONE) {
-    //         if (req.status === 200) {
-    //           resolve(req.response);
-    //         } else {
-    //           reject(req.response);
-    //         }
-    //       }
-    //     };
-    //   });
-    // };
+    this.auth.facebookCheck();
+    // this.navRoute.navigate(['/main']);
+  }
 
-    // const routeFunc = this.navRoute.navigate;
-
-    // FB.login(function(response){
-    //   if (response.status === 'connected') {
-    //     const fbtoken = response.authResponse.accessToken;
-    //     const userid = response.authResponse.userID;
-    //     // localStorage.setItem("currentUser", JSON.stringify({ fbtoken: fbtoken, userid: userid }));
-    //     // this.auth.facebooklogin(fbtoken,userid);
-    //     funcPost(fbtoken, userid)
-    //     .then(res => {
-    //       console.log(res);
-    //       localStorage.setItem('currentUser', JSON.stringify(res));
-    //       routeFunc(['/main']);
-    //     })
-    //     .then(res => {
-    //       console.log(res);
-    //     })
-    //     .catch(res => { console.log(res); });
-    //   }
-    // });
+  navigateMain() {
+    this.navRoute.navigate(['/main']);
   }
 }
