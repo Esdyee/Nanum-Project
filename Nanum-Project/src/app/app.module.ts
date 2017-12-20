69bb84548696f3e91f849e7d847350b3ee8e76fb
cmygray <cmygray@gmail.com>
2017-12-20 21:55:29 +0900

    main module에서 feed module 사용하도록 변경

--- Nanum-Project/src/app/app.module.ts
+++ Nanum-Project/src/app/app.module.ts
@@ -10,13 +10,10 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { TopicComponent } from './topic/topic.component';
 import { ProfileComponent } from './profile/profile.component';
-import { NavigatorComponent } from './common/navigator/navigator.component';
 import { AskModalComponent } from './common/navigator/ask-modal/ask-modal.component';
 import { NotFoundComponent } from './common/not-found/not-found.component';
 import { TopicSelectComponent } from './main-feed/topic-select/topic-select.component';
-import { NavTabComponent } from './common/nav-tab/nav-tab.component';
 import { ProfileFollowModalComponent } from './profile/profile-follow-modal/profile-follow-modal.component';
-import { MainLeftComponent } from './main/main-left/main-left.component';
 import { ProfileBottomModalComponent } from './profile/profile-bottom-modal/profile-bottom-modal.component';
 import { ProfileMiddleModalComponent } from './profile/profile-middle-modal/profile-middle-modal.component';
 import { ProfileTopModalComponent } from './profile/profile-top-modal/profile-top-modal.component';
@@ -32,12 +29,9 @@ import { SharedModule } from './shared/shared.module';
     LoginComponent,
     TopicComponent,
     ProfileComponent,
-    NavigatorComponent,
-    AskModalComponent,
     AskModalComponent,
     TopicSelectComponent,
     NotFoundComponent,
-    NavTabComponent,
     ProfileFollowModalComponent,
     ProfileBottomModalComponent,
     ProfileMiddleModalComponent,
--- Nanum-Project/src/app/main/main.module.ts
+++ Nanum-Project/src/app/main/main.module.ts
@@ -5,16 +5,24 @@ import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
 import { FeedModule } from '../shared/feed/feed.module';
 import { MainLeftComponent } from './main-left/main-left.component';
+import { NavigatorComponent } from '../common/navigator/navigator.component';
+import { FormsModule } from '@angular/forms';
+import { MaterialModule } from '../module/material.module';
+import { NavTabComponent } from '../common/nav-tab/nav-tab.component';
 
 @NgModule({
   imports: [
     CommonModule,
     MainRoutingModule,
-    FeedModule
+    FeedModule,
+    FormsModule,
+    MaterialModule
   ],
   declarations: [
     MainComponent,
-    MainLeftComponent
+    MainLeftComponent,
+    NavigatorComponent,
+    NavTabComponent
   ]
 })
 export class MainModule { }
