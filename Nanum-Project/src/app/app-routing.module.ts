1995b0f3b78d9e821715e79ef871463e7ee9d4ac
Esdyee <joimh@naver.com>
2017-12-20 17:06:19 +0900

    router 수정
    router 수정

--- Nanum-Project/src/app/app-routing.module.ts
+++ Nanum-Project/src/app/app-routing.module.ts
@@ -24,10 +24,6 @@ const routes: Routes = [
   },
   { path: 'profile', component: ProfileComponent, canActivate: [LoginGuard] },
   { path: 'maintest', component: MainComponent },
-  { path: '**', component: NotFoundComponent},
-  // 필수 추가 요망
-  // 메인피드 임시 path 입니다
-  { path: 'main', component: MainFeedComponent, canActivate: [LoginGuard] },
   { path: '**', component: NotFoundComponent}
 ];
 
